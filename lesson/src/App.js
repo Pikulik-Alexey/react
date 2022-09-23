a00ee2c6c7858f216dbe394fd8d4eb7cba76af11
Pikulik-Alexey <pikulik87@inbox.ru>
2022-09-23 20:26:31 +0500

    css

--- lesson/src/App.js
+++ lesson/src/App.js
@@ -1,50 +1,28 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import MessageList from './messageList/MessageList';
 
 function App() {
-  const sayStudy = "Я учусь на курсе по React";
+  const userName = "Алексей"
+  const theme = "light"
 
   return (
     <div className="App">
-      <Message sayStudy={sayStudy} />
+      <Myheader theme={theme} />
+      {/* <Myheader name={userName} id={1} /> */}
+      <MessageList />
     </div>
   );
 }
 
 export default App;
 
-const Message = ({ sayStudy }) => {
+// const Myheader = (props) => { }
 
+const Myheader = ({ theme }) => {
   return (
-    <div >
-      <h2 style={{ color: "#222222" }}>Привет, {sayStudy}</h2>
+    <div style={{ background: theme === "dark" ? "#282c34" : "#445444" }}>
+      <h2>Привет, Алексей</h2>
     </div >
   );
 }
-
-
-
-
-// function App() {
-//   const userName = "Алексей"
-//   const theme = "light"
-
-//   return (
-//     <div className="App">
-//       <Myheader theme={theme} />
-//       {/* <Myheader name={userName} id={1} /> */}
-//     </div>
-//   );
-// }
-
-// export default App;
-
-// // const Myheader = (props) => { }
-
-// const Myheader = ({ theme }) => {
-//   return (
-//     <div style={{ background: theme === "dark" ? "#282c34" : "#000000" }}>
-//       <h2>Привет, Алексей</h2>
-//     </div >
-//   );
-// }
--- lesson/src/messageList/messageList.js
+++ lesson/src/messageList/messageList.js
@@ -1,37 +1,81 @@
-import React, { useState } from 'react';
-import './messageList.css';
+import React, { useEffect, useState } from 'react';
+import './MessageList.css';
 
-function Msg() {
+
+function MessageList() {
+
+   const [messageList, setMessageList] = useState([])
+   const [messageBody, setMessageBody] = useState({
+      text: '',
+      autor: ''
+   })
+
+
+   const Robot_Message = 'Привет, я получил ваше сообщение!'
+
+   useEffect(() => {
+      if (messageList.length > 0 && messageList.slice(-1)[0].author !== 'robot') {
+         setTimeout(() => {
+            setMessageList(pervstate => [...pervstate, { text: Robot_Message, author: 'robot' }])
+         }, 1500)
+      }
+   }, [messageList])
 
    return (
-      <div className="Msg">
-         <Message />
+      <div className='msg'>
+         <Form data={messageBody} setData={setMessageBody} setMessage={setMessageList}></Form>
+         <div className='msgList'>
+            {
+               messageList.map((e, i) => <Message text={e.text} author={e.author} key={i} />)
+            }
+         </div>
       </div>
    );
 }
 
-export default Msg;
 
-const Message = (props) => {
+export default MessageList;
 
-   const [messageList, setMessageList] = useState([
-      {
-         text: "Я учусь на курсе по React",
-         author: "Алексей"
-      },
-      {
-         text: "Я учусь на курсе по React",
-         author: "Алексей",
-         computer: "Домашний"
+const Form = ({ data, setData, setMessage }) => {
+
+   const { text, author } = data
+
+   const submitForm = (e) => {
+      e.preventDefault()
+      if (text.length > 0) {
+         setMessage(pervstate => [...pervstate, { text, author }])
       }
-   ]
+      setData(
+         {
+            text: '',
+            author: ''
+         }
+      )
+   }
+
+   return (
+      <form onSubmit={submitForm}>
+         <input className='form' placeholder='Имя' value={text} onChange={(e) =>
+            setData(pervstate => ({ ...pervstate, text: e.target.value })
+            )} />
+
+         <input className='form' placeholder='Текст' value={author} onChange={(e) =>
+            setData(pervstate => ({ ...pervstate, author: e.target.value })
+            )} />
+
+         <button className='btn' type='submit'>Отправить</button>
+
+      </form>
    )
+}
+
+
+const Message = ({ author, text }) => {
 
    return (
       <div>
-         {
-            messageList.map(e => <div>{e}</div>)
-         }
+         <h1>{author}</h1>
+         <p>{text}</p>
       </div>
-   );
+   )
 }
